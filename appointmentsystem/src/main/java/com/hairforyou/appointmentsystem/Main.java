eda080566803064b0bf9fc1fa0a0a9ddfc792d29
Kimano Lambert <lambertkimano@gmail.com>
2023-03-29 16:31:00 -0500

    LoginForm multitab

--- appointmentsystem/src/main/java/com/hairforyou/appointmentsystem/Main.java
+++ appointmentsystem/src/main/java/com/hairforyou/appointmentsystem/Main.java
@@ -9,26 +9,35 @@ import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
 public class Main {
-    
-    public static void main(String[] args) {
-        try {
-            // Set System L&F
-            UIManager.setLookAndFeel(
-                UIManager.getSystemLookAndFeelClassName());
-        } 
-        catch (UnsupportedLookAndFeelException e) {
-           // handle exception
-        }
-        catch (ClassNotFoundException e) {
-           // handle exception
-        }
-        catch (InstantiationException e) {
-           // handle exception
-        }
-        catch (IllegalAccessException e) {
-           // handle exception
-        }
-    
-        new LoginSystem(); //Create and show the GUI.
-    }
+   public static void main(String args[]) {
+      /* Set the Nimbus look and feel */
+      //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+      /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+      * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
+      */
+      try {
+         // Set System L&F
+         UIManager.setLookAndFeel(
+            UIManager.getSystemLookAndFeelClassName());
+     }
+     catch (UnsupportedLookAndFeelException e) {
+        // handle exception
+     }
+     catch (ClassNotFoundException e) {
+        // handle exception
+     }
+     catch (InstantiationException e) {
+        // handle exception
+     }
+     catch (IllegalAccessException e) {
+        // handle exception
+     }
+
+      /* Create and display the form */
+      java.awt.EventQueue.invokeLater(new Runnable() {
+         public void run() {
+            new LoginForm().setVisible(true);
+         }
+      });
+   }
 }
